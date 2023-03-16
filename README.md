40754a2c3fcca860e122f45a79d3c4c3e658bc61
DevYouriD <youri.dera@gmail.com>
2023-03-17 00:22:43 +0100

    Updated README and script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Instructions
-Download the repository and extract the contents. Extract the folders and file onto your desktop. Download any image of type webp, and place it into the "Resources" folder. Run the script and your image will appear in the "Results" folder in jpeg format.
+1. (If not already done) install the pillow library with:
+
+```
+(sudo) pip3 install pillow
+```
+
+2. Download the repository and extract the contents. Extract the folders and file onto your desktop. Download any image of type webp, and place it into the "Resources" folder. Run the script and your image will appear in the "Results" folder in jpeg format.
--- WebpToJpgConverter.py
+++ WebpToJpgConverter.py
@@ -20,30 +20,3 @@ if not os.path.exists(targetDirectory):
     os.makedirs(targetDirectory)
 if os.path.exists(sourceDirectory) and os.path.exists(sourceDirectory):
     convertFiles()
-
-# GUI VERSION
-# import PIL.Image
-# from tkinter import *
-# import os
-
-# window = Tk()
-# #window.iconbitmap('Resources/[FILENAME].ico')
-# window.title('Image Converter')
-# window.config(height=500, width=500, background='black')
-
-# #MAIN FUNCTIONALITY
-# def convertImage():
-#     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-#     image = PIL.Image.open("Resources/test.webp").convert("RGB")
-#     image.save(desktop + "\\test.jpg", "jpeg")
-
-# Button(window, text='DO STUFF', command=convertImage, font='none 10 bold').place(relx=0.1, rely=0.2, anchor=CENTER)
-
-# #CLOSE
-# def close_window():
-#     window.destroy()
-#     exit()
-
-# Button(window, text='EXIT', width=6, command=close_window, font='none 10 bold').place(relx=0.08, rely=0.95, anchor=CENTER)
-
-# window.mainloop()
